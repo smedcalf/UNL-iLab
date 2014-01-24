5eef8acee79ceaeb4db71e0b49d057384a05294a
Dalton Dick <daltondick2010@gmail.com>
2014-01-23 18:48:36 -0600

    Add instructors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ilab::Application.routes.draw do
   
   resources :students
+  resources :instructors
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123014120) do
+ActiveRecord::Schema.define(version: 20140124002727) do
+
+  create_table "instructors", force: true do |t|
+    t.string   "email"
+    t.string   "classname"
+    t.string   "semester"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "students", force: true do |t|
     t.string   "email"
