02e264fc6e0457a300ee980946e71253cba57485
wangzhen0614 <wangzhen614@gmail.com>
2014-02-04 16:46:25 -0600

    fix minor issues of  register page and user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, except: [:new]
+	before_action :signed_in_user, except: [:new, :create]
 
 	def new
 		@user = User.new
