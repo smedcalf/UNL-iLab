c5cef64822db7e1be5d857f65e8197b4673d6d1d
wangzhen0614 <wangzhen614@gmail.com>
2014-02-04 16:54:33 -0600

    minor change of the user new (register page), enable redirect to user home page after register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			redirect_to user_path(@user.id)
+      sign_in(@user)
 		else
 			render 'new'
 		end
