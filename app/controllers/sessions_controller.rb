feaf2ad8491d556a56b7390f59fef7fe36924175
Dalton Dick <daltondick2010@gmail.com>
2014-02-11 16:25:49 -0600

    Login stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(name: params[:session][:name].downcase)
-		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to projects_path
+		if params[:session][:name] && params[:session][:password]
+			user = User.find_by(name: params[:session][:name].downcase)
+			if user && user.authenticate(params[:session][:password])
+				sign_in user
+				redirect_to projects_path
+			else
+				redirect_to root_url
+			end
 		else
-			render 'new'
+			redirect_to root_url
 		end
 	end
 
