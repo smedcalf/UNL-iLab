bcdc7f5947b8440f535d6dad1d6a96861813276e
wangzhen0614 <wangzhen614@gmail.com>
2014-02-25 16:44:57 -0600

    sponsor preference successfully implemented, and delete select margin to make it nicer.

--- app/controllers/sponsor_preferences_controller.rb
+++ app/controllers/sponsor_preferences_controller.rb
@@ -19,7 +19,38 @@ class SponsorPreferencesController < ApplicationController
   end
 
   def update
-    hkj
-    render text: params.to_s
+    params[:rating].each do |key, value|
+      @student_preference = StudentPreference.find(key.to_i)
+      student_id = @student_preference.student_id
+      project_id = @student_preference.project_id
+      rating = value.first.to_i
+      @sponsor_preference = SponsorPreference.where(:project_id => project_id, :student_id => student_id)
+      if(@sponsor_preference.nil?)
+        @sponsor_preference = SponsorPreference.new
+        @sponsor_preference.student_id = student_id
+        @sponsor_preference.project_id = project_id
+        @sponsor_preference.rating = rating
+        @sponsor_preference.save
+      else
+        @sponsor_preference.first.rating = rating
+        @sponsor_preference.first.save
+      end
+
+    end
+    #render text: params[:rating].to_s
+    flash[:success] = "Your rating was successfully saved!"
+    redirect_to :back
+
   end
+
+  private
+
+  def sponsor_preference_params
+    params.require(:sponsor_preference).permit(:student_id, :project_id, :rating)
+  end
+
+  def sponsor_student_preference
+    @sponsor_preference = SponsorPreference.find_by_id(params[:id])
+  end
+
 end
