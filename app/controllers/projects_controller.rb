2da80775b42168aeed43ad5c84142d918623cd55
wangzhen0614 <wangzhen614@gmail.com>
2014-04-04 13:08:58 -0500

    change before action in project controller, not allow sponsor create project or modify other projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
   before_action :signed_in_user
-  before_action :correct_project_owner, only: [:create, :new, :edit, :update, :manage_projects] ||
-      :signed_in_instructor, only: [:create, :new, :edit, :update, :manage_projects]
+  before_action :correct_project_owner || :signed_in_instructor, only: [:edit, :update]
+  before_action :signed_in_instructor, only:  [:new, :create, :manage_projects]
+  
   def index
     if current_user.sponsor.nil?
       @projects = Project.all
