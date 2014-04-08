d5f734d5b8de4a4051aee69599dece6d4b30382f
Dalton Dick <daltondick2010@gmail.com>
2014-04-08 15:36:10 -0500

    Instructor tests

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -15,10 +15,12 @@
 
 class Instructor < ActiveRecord::Base
 
+	belongs_to :user
+
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :email, presence: true
-	
+
 	def full_name
 		"#{self.first_name} #{self.last_name}"
 	end
--- spec/models/instructor_spec.rb
+++ spec/models/instructor_spec.rb
@@ -17,4 +17,48 @@ require 'spec_helper'
 
 describe Instructor do
 
+  before do
+    @instructor = Instructor.new(first_name: "Test", last_name: "Instructor", email: "test@instructor.com")
+  end
+
+  subject { @instructor }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:classname) }
+  it { should respond_to(:semester) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:user) }
+
+  it { should be_valid }
+
+  describe "first_name should be present" do
+    before do
+      @instructor.first_name = ""
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "last_name should be present" do
+    before do
+      @instructor.last_name = ""
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "email should be present" do
+    before do
+      @instructor.email = ""
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "full name" do
+    it "should be first_name + last_name" do
+      expect(@instructor.full_name).to eql("Test Instructor")
+    end
+  end
 end
