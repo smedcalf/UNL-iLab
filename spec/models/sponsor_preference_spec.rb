9c2f85eb2a1697be5764c5af8162202cac2c3c6b
Dalton Dick <daltondick2010@gmail.com>
2014-04-08 14:17:23 -0500

    Sponsor preferences tests

--- spec/models/sponsor_preference_spec.rb
+++ spec/models/sponsor_preference_spec.rb
@@ -13,5 +13,35 @@
 require 'spec_helper'
 
 describe SponsorPreference do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:student) { FactoryGirl.create(:student, user: user) }
+  let(:project) { FactoryGirl.create(:project) }
 
+  before do
+    @sp = SponsorPreference.new(student: student, project: project, rating: 4)
+  end
+
+  subject { @sp }
+
+  it { should respond_to(:student) }
+  it { should respond_to(:project) }
+  it { should respond_to(:rating) }
+
+  it { should be_valid }
+
+  describe "student should be present" do
+    before do
+      @sp.student = nil
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "project should be present" do
+    before do
+      @sp.project = nil
+    end
+
+    it { should_not be_valid }
+  end
 end
