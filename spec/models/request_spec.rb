5249e9c97f01cf515f07ff1e437f5751a6e572f1
Dalton Dick <daltondick2010@gmail.com>
2014-04-15 16:06:54 -0500

    Request specs

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -15,5 +15,32 @@
 require 'spec_helper'
 
 describe Request do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @request = Request.new(start_date: DateTime.now, end_date: DateTime.now + 1.hour)
+  end
+
+  subject { @request }
+
+  it { should respond_to(:student) }
+  it { should respond_to(:start_date) }
+  it { should respond_to(:end_date) }
+  it { should respond_to(:approved) }
+  it { should respond_to(:active) }
+
+  it { should be_valid }
+
+  describe "#setDefaults" do
+    before do
+      @r = Request.new(start_date: DateTime.now, end_date: DateTime.now + 1.hour)
+    end
+
+    it "should have approved set" do
+      expect(@r.approved).to eql(false)
+    end
+
+    it "should have active set" do
+      expect(@r.active).to eql(true)
+    end
+  end
 end
